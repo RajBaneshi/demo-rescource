61b51c89009f752466d536d88dcef649108fbffe
Rajendra Singh <32132076+RajBaneshi@users.noreply.github.com>
2024-12-02 15:19:13 +0530

    ansible vm creation

--- main.tf
+++ main.tf
@@ -1,42 +1,38 @@
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = var.location
-  }
+}
 
-
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "example" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id # Associate Public IP
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
-
-# Associate NSG with NIC
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.main.id
+  network_interface_id      = azurerm_network_interface.example.id
   network_security_group_id = azurerm_network_security_group.main.id
 }
-
 # Define a Network Security Group (NSG)
 resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-nsg"
@@ -78,60 +74,45 @@ resource "azurerm_public_ip" "example" {
     environment = "Production"
   }
 }
+resource "tls_private_key" "linux_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+# We want to save the private key to our machine
+# We can then use this key to connect to our Linux VM
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+resource "local_file" "linuxkey" {
+  filename="linuxkey.pem"  
+  content=tls_private_key.linux_key.private_key_pem 
+}
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+  
 
-  storage_image_reference {
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.linux_key.public_key_openssh
+  }
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-  
 }
-# # Data Disk
-# resource "azurerm_managed_disk" "data_disk" {
-#   name                 = "example-data-disk"
-#   location             = azurerm_resource_group.example.location
-#   resource_group_name  = azurerm_resource_group.example.name
-#   storage_account_type = "Standard_LRS"
-#   disk_size_gb         = 10
-#   create_option = "Empty"
-# }
-
-# # Attach the Data Disk to the VM
-# resource "azurerm_virtual_machine_data_disk_attachment" "disk_attachment" {
-#   managed_disk_id    = azurerm_managed_disk.data_disk.id
-#   virtual_machine_id = azurerm_virtual_machine.main.id
-#   lun                = 0 # Logical Unit Number for the disk
-#   caching            = "ReadWrite"
-# }
